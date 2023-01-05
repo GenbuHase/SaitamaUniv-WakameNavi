ae072cdc6ab9c9036d5582790f302065a4546d24
どっかのげんちゃん。 <14960841+GenbuHase@users.noreply.github.com>
2023-01-05 22:37:47 +0900

    0.1.5 $ server.ts → main.ts

--- src/public/index.js
+++ src/public/index.js
@@ -1,1 +1,19 @@
-console.log("loaded");
+const TEST = [
+  {
+    route: "北浦03",
+    destination: "（埼大通り発）北浦和駅西口",
+    location: 1,
+    plannedTime: "22:19",
+    arrivalTime: "22:19",
+    delay: 0
+  },
+
+  {
+    route: "北朝02",
+    destination: "南与野駅西口",
+    location: 3,
+    plannedTime: "08:30",
+    arrivalTime: "08:58",
+    delay: 28
+  }
+];
