d4b6eb4ef80d22cc5252dd3d2f76842c4a5e0b4d
どっかのげんちゃん。 <14960841+GenbuHase@users.noreply.github.com>
2023-01-08 14:38:54 +0900

    0.2.5 + Vuetifyを導入 + .gitignoreを追記 + index.htmlのフォームを動作するように変更

--- app.vue
+++ app.vue
@@ -1,5 +1,18 @@
 <template>
   <div>
-    
+    <v-container>
+      <v-btn
+        depressed
+        color="primary"
+      >
+        Primary
+      </v-btn>
+      <v-btn
+        depressed
+        color="error"
+      >
+        Error
+      </v-btn>
+    </v-container>
   </div>
 </template>
