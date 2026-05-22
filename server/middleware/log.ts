4fc7a29b28eda8848118f83b883b01ae727504a8
Genbu Hase <14960841+GenbuHase@users.noreply.github.com>
2026-05-23 06:11:29 +0900

    alpha-2.9.5 + ログ出力を改善

--- server/middleware/log.ts
+++ server/middleware/log.ts
@@ -1,5 +1,10 @@
 export default defineEventHandler(event => {
   const { req, res } = event.node;
+  const start = Date.now();
 
-  console.log(`${req.method}: ${req.url}`, event);
+  res.on('finish', () => {
+    const duration = Date.now() - start;
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url} - ${res.statusCode} (${duration}ms)`);
+  });
 });
+
